d55a17e7288c0ab56d0623d152972898e25cd6d3
Matt Bierner <matb@microsoft.com>
2019-01-07 18:34:14 -0800

    Adding pinning test for language identifiers

---
+++ test/colorize-fixtures/language-identifiers.js
@@ -0,0 +1,36 @@
+const yes = /* js */`
+    1 + 1
+`;
+
+const yes = /*js*/`
+    1 + 1
+`;
+const yes = /*js*/   `
+    1 + 1
+`;
+
+const yes = /*               js                 */`
+    1 + 1
+`;
+
+const yes = /*               js                 */                    `
+    1 + 1
+`;
+
+
+const no = /* js */
+`
+    1 + 1
+`;
+
+const no = /* js something */`
+    1 + 1
+`;
+
+const no = /* jsomething */`
+    1 + 1
+`;
+
+const no = /* something js */`
+    1 + 1
+`;
